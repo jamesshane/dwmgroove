f1ddc87478337847e765c75bda1a31c7d7ccbb5e
James Shane <jamesshane@gmail.com>
2020-02-21 10:32:56 +0000

    created install_ubuntu.sh

---
+++ install_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#remove bar
+sudo rm /usr/local/bin/dwmgrooveBar.sh
+
+#add bar
+sudo cp dwmgrooveBar.sh /usr/local/bin/
+sudo chmod +x /usr/local/bin/dwmgrooveBar.sh
+
+echo "Done!"
+
